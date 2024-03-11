4e2152d89620aaab79c1702e76f76cd6201e76cc
Chatoyant19 <chatoyant_s_email@163.com>
2024-03-11 11:52:49 +0800

    commit

--- src/dr_lidar_calib/include/show_tools.h
+++ src/dr_lidar_calib/include/show_tools.h
@@ -8,4 +8,7 @@ namespace show_tools {
 cv::Mat getConnectImg(const pcl::PointCloud<pcl::PointXYZ>::Ptr& rgb_edge_cloud,
                       const pcl::PointCloud<pcl::PointXYZ>::Ptr& depth_edge_cloud,
                       const int& dis_threshold, const int& camera_width, const int& camera_height);
+cv::Mat getProjectionImg(const cv::Mat& raw_img, const pcl::PointCloud<pcl::PointXYZI>::Ptr& raw_point,
+                         const Eigen::Matrix4d& Tx_C_L,
+                         const cv::Mat& camera_matrix, cv::Mat& distortion_coeff);                                           
 } // namespace show_tools
